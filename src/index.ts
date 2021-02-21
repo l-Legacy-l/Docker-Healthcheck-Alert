f38cd53fd17acdc34c26890b0d90fc40f6f9af09
Fabio Cumbo <fcumbo@cirb.brussels>
2021-02-21 22:38:34 +0100

    Send alert mail when container is unhealthy + add a verification to avoid sending an alert if there is no healthcheck

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
-// import { Container } from './model/DockerResponse';
-// const http = require('http');
-
 import axios, { AxiosRequestConfig } from 'axios';
 import { Container, ContainerDetails, HEALTH_STATUS } from './model/DockerResponse';
+const nodemailer = require('nodemailer');
 
 
 const config: AxiosRequestConfig = {
   socketPath: '/var/run/docker.sock'
 }
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_FROM,
+    pass: process.env.PASSWORD
+  }
+});
+
+const mailOptions = {
+  from: process.env.MAIL_FROM,
+  to: process.env.MAIL_TO,
+  subject: 'Docker Heathcheck Alert - Unhealthy container reported',
+  text: ''
+};
+
 
 let unHealthyContainers: string[] = [];
 
@@ -31,18 +44,32 @@ async function main() {
 
   for (const container of containers) {
     const {Id, Health}: ContainerDetails = await request('/containers/' + container.Id + '/json');
-
-    if (Health?.Status !== HEALTH_STATUS.healthy &&
-      !unHealthyContainers.find((unHealthyContainer) => unHealthyContainer === Id)) {
-
-      unHealthyContainers.push(Id);
-    } else if (Health?.Status === HEALTH_STATUS.healthy &&
-      unHealthyContainers.find((unHealthyContainer) => unHealthyContainer === Id)) {
-
-      unHealthyContainers.filter((unHealthyContainer) => unHealthyContainer !== Id);
+    if(Health?.Status) {
+      if (Health.Status !== HEALTH_STATUS.healthy &&
+        !unHealthyContainers.find((unHealthyContainer) => unHealthyContainer === Id)) {
+  
+        unHealthyContainers.push(Id);
+        sendAlert(Id);
+  
+      } else if (Health.Status === HEALTH_STATUS.healthy &&
+        unHealthyContainers.find((unHealthyContainer) => unHealthyContainer === Id)) {
+  
+        unHealthyContainers.filter((unHealthyContainer) => unHealthyContainer !== Id);
+      }
     }
 
   }
+}
+
+function sendAlert(Id: string) {
+  mailOptions.text= 'Unhealthy container: ' + Id;
+  transporter.sendMail(mailOptions, (error: string, info: {response: string}) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
 }
 
 main();
